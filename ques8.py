bfe216ee27332b4b4756f8e0a63f5d9bb6fdcb8c
LikhithaBR <likhitharamb@gmail.com>
2022-06-12 17:20:04 +0530

    ques8

---
+++ ques8.py
@@ -0,0 +1,18 @@
+
+list1 = [1, 11, 7, 8, -1, 6 ,3]
+def avg(list1):
+    total = 0
+    if(len(list1 )!=0):
+
+        for ele in range(0, len(list1)):
+            total = total + list1[ele]
+
+    else:
+        return 0
+
+    average =total /len(list1)
+    return average
+
+
+
+print(avg(list1))
